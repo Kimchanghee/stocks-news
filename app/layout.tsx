344d1b1ed4101951999636fc4f533d78cbffad58
aicompany <aicp0101@gmail.com>
2026-05-11 01:09:59 +0900

    feat(analytics): GA4 G-G0YE8ZCN66

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G0YE8ZCN66" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-G0YE8ZCN66',{page_path:window.location.pathname});",
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
