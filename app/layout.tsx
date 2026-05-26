39478a705a53d1c7f0058bfc625d612953c042fe
Kimchanghee <9594198+Kimchanghee@users.noreply.github.com>
2026-05-26 15:00:05 +0900

    Fix shared GA4 measurement ID to G-G0YE8ZCN66

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3LYD80FR0D" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G0YE8ZCN66" />
         <script
           dangerouslySetInnerHTML={{
-            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-3LYD80FR0D',{page_path:window.location.pathname});",
+            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-G0YE8ZCN66',{page_path:window.location.pathname});",
           }}
         />
       </head>
