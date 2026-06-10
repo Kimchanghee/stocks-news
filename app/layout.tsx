1f0f07b9a4dd3dac2516e5defaa0349fdf09500a
Kimchanghee <9594198+Kimchanghee@users.noreply.github.com>
2026-06-11 04:11:32 +0900

    Switch StockMarketNews GA4 tag to site property

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3LYD80FR0D" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-M0JVEG23XY" />
         <script
           dangerouslySetInnerHTML={{
-            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-3LYD80FR0D',{page_path:window.location.pathname});",
+            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-M0JVEG23XY',{page_path:window.location.pathname});",
           }}
         />
       </head>
